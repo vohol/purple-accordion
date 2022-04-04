145391813e98d78047d1e6490736d0938cf42f4a
Kholiavskiy Vladimir <vohol@pop-os.localdomain>
2022-04-04 14:04:22 +0300

    fixed padding

--- src/js/app.js
+++ src/js/app.js
@@ -16,13 +16,17 @@ function tabsOpenClose(event) {
     //close faq item when it is active
     if (target.closest(summaryClass).parentNode.classList.contains('active')) {
       target.closest(summaryClass).parentNode.classList.remove('active');
+      target.closest(summaryClass).parentNode.children[1].style.height = '0'
     } 
     else {
       listItems.forEach(element => {
         element.classList.remove('active')
+        element.children[1].style.height = '0'
       });
       
       target.closest(summaryClass).parentNode.classList.add('active');
+      target.closest(summaryClass).parentNode.children[1].style.height = target.closest(summaryClass).parentNode.children[1].children[0].offsetHeight + 'px'
+      
     }
   }
 }
